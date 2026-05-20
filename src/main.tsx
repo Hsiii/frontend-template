65631cf127882b0f0c864668ce1936ffbe77868c
Hsi <its.hsichen@gmail.com>
2026-05-20 23:37:41 +0800

    refactor: place app entry in /component

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import ReactDOM from 'react-dom/client';
 
-import { App } from './App.js';
+import { App } from './components/App.js';
 
 import './index.css';
 
